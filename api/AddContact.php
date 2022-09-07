ea52f19c6a10bee67216328942ba59cb6cbc375e
Robert Boyd III <git@robertboyd.dev>
2022-09-07 18:32:40 -0400

    Fix AddContact endpoint to work properly

--- api/AddContact.php
+++ api/AddContact.php
@@ -1,15 +1,20 @@
 <?php
 require_once('./Utils.php');
 on_json_request_with_db(function(mixed $request_data, mysqli $db) {
-	$contact = $request_data['contact'];
 	$user_id = $request_data['userId'];
+	$contact = $request_data['contact'];
 
-	$stmt = $db->prepare('INSERT into Contacts (UserId,Name) VALUES(?,?)');
-	$stmt->bind_param('ss', $user_id, $contact);
-	$stmt->execute();
+	$first_name = $contact['firstName'];
+	$last_name = $contact['lastName'];
+	$email = $contact['email'];
+	$phone_number = $contact['phoneNumber'];
+
+	$stmt = $db->prepare('INSERT into Contacts (UserID, FirstName, LastName, Email, PhoneNumber) VALUES(?,?,?,?,?)');
+	$stmt->bind_param('ss', $user_id, $first_name, $last_name, $email, $phone_number);
+	$success = $stmt->execute();
 	$stmt->close();
 
 	return [
-		'hello' => 'world'
+		"result" => $success
 	];
 });

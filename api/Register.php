6d205b86677479ee5dbd5623ef09135773eb8b37
Kyle <112042253+kylekww@users.noreply.github.com>
2022-09-07 20:17:32 -0400

    Update Register.php

--- api/Register.php
+++ api/Register.php
@@ -1,27 +1,30 @@
 <?php
-$username = $request_data['username'];
-$stmt = $db->prepare("select * from Users where Login = ?");
-$stmt->bind_param("s", $username);
-if ($stmt->execute())
-{
-  return [
-    'result' => false,
-    'error' => 'Username already taken.'
-  ];
+require_once('./Utils.php');
+on_json_request_with_db(function (mixed $requestData, mysqli $db) {
+  $username = $request_data['username'];
+  $stmt = $db->prepare("select * from Users where Login = ?");
+  $stmt->bind_param("s", $username);
+  if ($stmt->execute())
+  {
+    $stmt->close();
+    return [
+      'result' => false,
+      'error' => 'Username already taken.'
+    ];
+  }
+
+  $hashed_password = password_hash($request_data["password"], PASSWORD_DEFAULT);
+  $user_id = $request_data['user_id'];
+  $first_name = $request_data['first_name'];
+  $last_name = $request_data['last_name'];
+  $login = $request_data['login'];
+
+
+  $stmt = $db->prepare('INSERT into Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?,?)');
+  $stmt->bind_param('ssss', $first_name, $last_name, $login, $hashed_password);
+  $success = $stmt->execute();
 }
-
-$hashed_password = password_hash($request_data["password"], PASSWORD_DEFAULT);
-$user_id = $request_data['user_id'];
-$first_name = $request_data['first_name'];
-$last_name = $request_data['last_name'];
-$login = $request_data['login'];
-
-
-$stmt = $db->prepare('INSERT into Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?,?)');
-$stmt->bind_param('ssss', $first_name, $last_name, $login, $hashed_password);
-$success = $stmt->execute();
-$stmt->close();
-
+                        
 return [
   "result" => $success
 ];

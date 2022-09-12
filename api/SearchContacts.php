ceaa01c9b81d44a823983092870037f7354ae14c
Robert Boyd III <git@robertboyd.dev>
2022-09-12 19:37:48 -0400

    Fix casing for SearchContacts

--- api/SearchContacts.php
+++ api/SearchContacts.php
@@ -12,6 +12,17 @@ on_json_request_with_db(function (mixed $requestData, mysqli $db) {
 		array_push($contacts, $row);
 	}
 
+	$contacts_with_fixed_casing = [];
+	foreach ($contacts as $c) {
+		array_push($contacts_with_fixed_casing, [
+			'contactId' => $c['ContactID'],
+			'firstName' => $c['FirstName'],
+			'lastName' => $c['LastName'],
+			'email' => $c['Email'],
+			'phoneNumber' => $c['PhoneNumber'],
+		]);
+	}
+
 	$stmt->close();
 	$db->close();
 

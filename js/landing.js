bfbba53cb5f7759bb720d672d216f055dcdb90ab
Robert Boyd III <git@robertboyd.dev>
2022-09-19 21:30:26 -0400

    Use data from api instead of hardcoded data

--- js/landing.js
+++ js/landing.js
@@ -1,196 +1,38 @@
-async function loadIntoTable() {
-	const data = await fetch("/api/SearchContacts.php", {
+async function searchContacts(query, userId) {
+	return await fetch("/api/SearchContacts.php", {
 		method: "POST",
 		headers: {
 			"content-type": "application/json",
 		},
 		body: JSON.stringify({
-			query: "rob",
-			userId: 1,
+			query,
+			userId,
 		})
-	}).then(b => b.text());
-	console.log(data);
+	}).then(b => b.json());
 }
-loadIntoTable();
-const data = {
-	contacts: [
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-		{
-			firstName: "Rob",
-			lastName: "Boyd",
-			email: "abc123@robertboyd.dev",
-			phone: "123456789"
-		},
-	]
-};
 
-let tab = 
-        `<tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Email</th>
-          <th>Phone Number</th>
-         </tr>`;
-    
-    // Loop to access all rows 
-    for (let r of data.contacts.slice(0, 10)) {
-        tab += `<tr> 
-    <td>${r.firstName} </td>
-    <td>${r.lastName}</td>
-    <td>${r.email}</td> 
-    <td>${r.phone}</td>          
+(async () => {
+	const data = await searchContacts("", 1);
+	console.log(data);
+
+	let tab = 
+		`<tr>
+<th>First Name</th>
+<th>Last Name</th>
+<th>Email</th>
+<th>Phone Number</th>
 </tr>`;
-    }
-    // Setting innerHTML as tab variable
-    document.getElementById("contacts").innerHTML = tab;    
-    
-//}
+
+	// Loop to access all rows 
+	for (let contact of data.contacts.slice(0, 10)) {
+		const { firstName, lastName, email, phoneNumber } = contact;
+		tab += `<tr> 
+<td>${firstName} </td>
+<td>${lastName}</td>
+<td>${email}</td> 
+<td>${phoneNumber}</td>          
+</tr>`;
+	}
+	// Setting innerHTML as tab variable
+	document.getElementById("contacts").innerHTML = tab;    
+})();

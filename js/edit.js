79334fcced8349b2ad36b904be3385122e9806f5
joshfeds <84419552+joshfeds@users.noreply.github.com>
2022-09-22 18:58:01 -0400

    trying to get updating server to work

--- js/edit.js
+++ js/edit.js
@@ -1,38 +1,42 @@
 
 async function editContacts(){
-    const data = await fetch("/api/SearchContacts.php", {
-		method: "POST",
-		headers: {
-			"content-type": "application/json",
-		},
-		body: JSON.stringify({
-			query: "rob",
-			userId: 1,
-		})
-	}).then(b => b.text());
+const data = await fetch("/api/SearchContacts.php", {
+    method: "POST",
+    headers: {
+        "content-type": "application/json",
+    },
+    body: JSON.stringify({
+        query: "rob",
+        userId: 1,
 
-    const first = document.getElementById("firstName");
+    })
+}).then(b => b.json());
+    /*console.log(data.contacts[0].lastName);
+    console.log(data.contacts[0].email);
+    console.log(data.contacts[0].phoneNumber);
+    console.log(data.contacts[0].contactId);*/
+    
+    const first = document.getElementById("firstName").value;
     const last = document.getElementById("lastName");
     const mail = document.getElementById("email");
     const tele = document.getElementById("phoneNumber");
-    console.log("before");
-    console.log(data);
     
-    /*if(first.localeCompare(firstName) != 0 && first.length > 0){
-        firstName = first;
+    if(first != data.contacts[0].firstName && first.length > 0){
+    	console.log("they are different!");
+        fetch("/api/SearchContacts.php", {
+    		method: "PATCH",
+    		headers: {
+        		"content-type": "application/json",
+    		},
+    		body: JSON.stringify({
+       	 		"firstName": first
+    		})
+	}).then(b => b.json())
     }
-
-    if(last.localeCompare(`${lastName}`) != 0 && last.length > 0){
-        lastName = last;
+        
+  else{
+    console.log("same");
     }
+        
 
-    if(mail.localeCompare(`${email}`) != 0 && mail.length > 0){
-        email = mail;
-    }
-
-    if(tele.localeCompare(`${phone}`) != 0 && tele.length > 0){
-        phoneNumber = tele;
-    }*/
-    console.log("after");
-    console.log(data);
 }

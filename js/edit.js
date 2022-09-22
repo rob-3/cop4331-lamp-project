fb2d5e34eacc4d5536e8b30be973146c5612ab70
joshfeds <84419552+joshfeds@users.noreply.github.com>
2022-09-21 23:44:52 -0400

    commented out code since it doesnt work

--- js/edit.js
+++ js/edit.js
@@ -1,44 +1,38 @@
-let currentRequestId = 0;
-
-async function getContacts(query, userId){
-    return {
-        userId: 1,
-        contacts: [
-            {
-                contactId: 1,
-                firstName: "Chad",
-                lastName: "Chaddington",
-                email: "chopping@jacks.com",
-                phone: "3216650987"
-            },
-        ]
-    };
-}
 
 async function editContacts(){
-    const data = await getContacts(query, 1)
-    const { firstName, lastName, email, phoneNumber } = contact;
-    const first = document.getElementById(firstName);
-    const last = document.getElementById(lastName);
-    const mail = document.getElementById(email);
-    const tele = document.getElementById(phoneNumber);
+    const data = await fetch("/api/SearchContacts.php", {
+		method: "POST",
+		headers: {
+			"content-type": "application/json",
+		},
+		body: JSON.stringify({
+			query: "rob",
+			userId: 1,
+		})
+	}).then(b => b.text());
+
+    const first = document.getElementById("firstName");
+    const last = document.getElementById("lastName");
+    const mail = document.getElementById("email");
+    const tele = document.getElementById("phoneNumber");
     console.log("before");
     console.log(data);
-    if(localeCompare(first, `${firstName}`) != 0 && first.length > 0){
-        `${firstName}` = first;
+    
+    /*if(first.localeCompare(firstName) != 0 && first.length > 0){
+        firstName = first;
     }
 
-    if(localeCompare(last, `${lastName}`) != 0 && last.length > 0){
-        `${firstName}` = first;
+    if(last.localeCompare(`${lastName}`) != 0 && last.length > 0){
+        lastName = last;
     }
 
-    if(localeCompare(mail, `${email}`) != 0 && mail.length > 0){
-        `${email}` = mail;
+    if(mail.localeCompare(`${email}`) != 0 && mail.length > 0){
+        email = mail;
     }
 
-    if(localeCompare(tele, `${phone}`) != 0 && tele.length > 0){
-        `${phoneNumber}` = tele;
-    }
+    if(tele.localeCompare(`${phone}`) != 0 && tele.length > 0){
+        phoneNumber = tele;
+    }*/
     console.log("after");
     console.log(data);
 }

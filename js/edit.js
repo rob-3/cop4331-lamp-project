a9ff90c5533bde123e57019ba9d34ab3c422ea81
joshfeds <84419552+joshfeds@users.noreply.github.com>
2022-09-23 15:38:12 -0400

    editing function to allow first,last,etc.

--- js/edit.js
+++ js/edit.js
@@ -1,50 +1,66 @@
 
 async function editContacts(){
-    const data = await fetch("/api/SearchContacts.php", {
-        method: "POST",
-        headers: {
-            "content-type": "application/json",
-        },
-        body: JSON.stringify({
-            query: "rob",
-            userId: 1,
-    
-        })
-    }).then(b => b.json());
-        /*console.log(data.contacts[0].lastName);
-        console.log(data.contacts[0].email);
-        console.log(data.contacts[0].phoneNumber);
-        console.log(data.contacts[0].contactId);*/
-        
-        const first = document.getElementById("firstName").value;
-        const last = document.getElementById("lastName");
-        const mail = document.getElementById("email");
-        const tele = document.getElementById("phoneNumber");
-        
-        if(first != data.contacts[0].firstName && first.length > 0){
-          console.log("they are different!"); 
-               fetch("/api/EditContact.php", {
-                  method: "POST",
-                  headers: {
-                    "content-type": "application/json",
-                  },
-                  body: JSON.stringify({
-                    userId: 1,
-                    contact: {
-                        contactId: 567570,
-                        firstName: 'Josh',
-                        lastName: 'Feds',
-                        email: 'hello@gmail.com',
-                        phoneNumber: '123456789'
-                    }
-                  })
-          }).then(b => b.json());  
-    
-        }
-            
-      else{
-        console.log("same");
-        }
-        console.log(data);
+  const data = await fetch("/api/SearchContacts.php", {
+      method: "POST",
+      headers: {
+          "content-type": "application/json",
+      },
+      body: JSON.stringify({
+          query: "rob",
+          userId: 1,
+  
+      })
+  }).then(b => b.json());
       
-     }
+      const first = document.getElementById("firstName").value;
+      const last = document.getElementById("lastName").value;
+      const mail = document.getElementById("email").value;
+      const tele = document.getElementById("phoneNumber").value;
+      
+      if(first != data.contacts[0].firstName && first.length > 0){
+        console.log("they are different!");
+      }
+       
+    else first = data.contacts[0].firstName;
+    
+
+    if(last != data.contacts[0].lastName && last.length > 0)
+      console.log("they are different!");
+    
+     
+  else
+      last = data.contacts[0].lastName;
+  
+
+  if(mail != data.contacts[0].email && mail.length > 0){
+    console.log("they are different!");
+  }
+   
+else{
+    mail = data.contacts[0].mail;
+}
+
+  if(tele != data.contacts[0].phoneNumber && tele.length > 0){
+    console.log("they are different!");
+}
+ 
+  else{
+    tele = data.contacts[0].phoneNumber;
+}
+    fetch("/api/EditContact.php", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        userId: 1,
+        contact: {
+            contactId: 567570,
+            firstName: first,
+            lastName: last,
+            email: mail,
+            phoneNumber: tele
+        }
+      })
+}).then(b => b.json());
+   }

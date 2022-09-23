85c1cca1fa5033a3a6a52a1aa11e925d3dda27d6
joshfeds <84419552+joshfeds@users.noreply.github.com>
2022-09-23 14:28:40 -0400

    data being deleted for unknown reason

--- js/edit.js
+++ js/edit.js
@@ -1,45 +1,50 @@
 
 async function editContacts(){
-const data = await fetch("/api/SearchContacts.php", {
-    method: "POST",
-    headers: {
-        "content-type": "application/json",
-    },
-    body: JSON.stringify({
-        query: "rob",
-        userId: 1,
-
-    })
-}).then(b => b.json());
-    /*console.log(data.contacts[0].lastName);
-    console.log(data.contacts[0].email);
-    console.log(data.contacts[0].phoneNumber);
-    console.log(data.contacts[0].contactId);*/
-    
-    const first = document.getElementById("firstName").value;
-    
-    if(first != data.contacts[0].firstName && first.length > 0){
-            console.log("they are different!"); 
-               
-}
-        
-  else{
-    console.log("same");
-    }
-        
-  console.log(data.contacts[0]); 
-}
-
-async function deleteContact({ userId, contactId }) {
-    const data = await fetch("/api/DeleteContact.php", {
-        method: "DELETE",
+    const data = await fetch("/api/SearchContacts.php", {
+        method: "POST",
         headers: {
             "content-type": "application/json",
         },
         body: JSON.stringify({
-            userId,
-            contactId,
+            query: "rob",
+            userId: 1,
+    
         })
     }).then(b => b.json());
-    return data.result;
-}
+        /*console.log(data.contacts[0].lastName);
+        console.log(data.contacts[0].email);
+        console.log(data.contacts[0].phoneNumber);
+        console.log(data.contacts[0].contactId);*/
+        
+        const first = document.getElementById("firstName").value;
+        const last = document.getElementById("lastName");
+        const mail = document.getElementById("email");
+        const tele = document.getElementById("phoneNumber");
+        
+        if(first != data.contacts[0].firstName && first.length > 0){
+          console.log("they are different!"); 
+               fetch("/api/EditContact.php", {
+                  method: "POST",
+                  headers: {
+                    "content-type": "application/json",
+                  },
+                  body: JSON.stringify({
+                    userId: 1,
+                    contact: {
+                        contactId: 567570,
+                        firstName: 'Josh',
+                        lastName: 'Feds',
+                        email: 'hello@gmail.com',
+                        phoneNumber: '123456789'
+                    }
+                  })
+          }).then(b => b.json());  
+    
+        }
+            
+      else{
+        console.log("same");
+        }
+        console.log(data);
+      
+     }

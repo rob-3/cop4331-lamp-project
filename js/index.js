8f42e147e1a243856c3e793808349dcf181c6999
joshfeds <84419552+joshfeds@users.noreply.github.com>
2022-09-27 00:15:13 -0400

    code for error/success messages

--- js/index.js
+++ js/index.js
@@ -1,31 +1,46 @@
 async function doLogin() {
-  const username = document.getElementById("loginName").value;
-  const password = document.getElementById("loginPassword").value;
-
-  const data = await fetch("/api/Login.php", {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify({
-      username,
-      password,
-    }),
-  }).then((b) => b.json());
-  if (data.result) {
-    console.log(
-      "Your name is " +
-        data.user.firstName +
-        " " +
-        data.user.lastName +
-        " and you have logged in."
-    );
-    localStorage.setItem('user', JSON.stringify(data.user));
-    window.location.href = "LandingPage.html";
-  } else console.log(data.error);
-}
-
-function doLogout() {
-  localStorage.clear();
-  window.location.href = "index.html";
-}
+    const username = document.getElementById("loginName").value;
+    const password = document.getElementById("loginPassword").value;
+  
+    const data = await fetch("/api/Login.php", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    }).then((b) => b.json());
+    if (data.result) {
+      console.log(
+        "Your name is " +
+          data.user.firstName +
+          " " +
+          data.user.lastName +
+          " and you have logged in."
+      );
+      localStorage.setItem('user', JSON.stringify(data.user));
+      var test = document.getElementById('error');
+      if(test){
+        document.getElementById('error').id = 'success'; 
+        
+      } else document.getElementById('noerror').id = 'success';
+      
+      const text = document.getElementById('success');
+      text.textContent = "Success! Logging in..."  
+      window.location.href = "LandingPage.html";
+    }else{
+        var test = document.getElementById('noerror');
+        if(test){
+          document.getElementById('noerror').id = 'error';         
+        }        
+       console.log(data.error);
+       }
+  }
+  
+  function doLogout() {
+    localStorage.clear();
+    window.location.href = "index.html";
+  }
+  

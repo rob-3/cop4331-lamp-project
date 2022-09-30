1a8f3458248f2943bc8b3416b1b9f6f34078855d
Baymax143-stack <59593004+Baymax143-stack@users.noreply.github.com>
2022-09-29 21:10:33 -0400

    Last Attempt to Fix the Loading Page

--- js/LandingPage.js
+++ js/LandingPage.js
@@ -4,36 +4,30 @@ let logo = document.querySelector('.logo-header');
 let logoSpan = document.querySelectorAll('.logo');
 
 window.addEventListener('DOMContentLoaded', ()=>{
+    setTimeout(()=>{
 
-    if(!sessionStorage.getItem('shown')) {
+      logoSpan.forEach((span, idx)=>{
+          setTimeout(()=>{
+              span.classList.add('active');
+          }, (idx + 1) * 400)
+      });
+
       setTimeout(()=>{
+          logoSpan.forEach((span, idx)=>{
 
-        logoSpan.forEach((span, idx)=>{
-            setTimeout(()=>{
-                span.classList.add('active');
-            }, (idx + 1) * 400)
-        });
+              setTimeout(()=>{                   
+                 span.classList.remove('active');
+                  span.classList.add('fade');
+               }, (idx + 1) * 50)
+          })
+      },2000);
 
-        setTimeout(()=>{
-            logoSpan.forEach((span, idx)=>{
+      setTimeout(()=>{
+          splash.style.top = '-100vh';
+      },2800);
 
-                setTimeout(()=>{
-                    span.classList.remove('active');
-                    span.classList.add('fade');
-                }, (idx + 1) * 50)
-            })
-        },2000);
-
-        setTimeout(()=>{
-            splash.style.top = '-100vh';
-        },2800);
-
-      })
-    }
-
-    sessionStorage.setItem('shown', true); 
+    })
 })
-
 
 /* Other Functions */
 /*
